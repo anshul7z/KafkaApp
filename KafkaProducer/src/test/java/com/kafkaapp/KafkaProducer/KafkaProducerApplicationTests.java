b043f8c3e470d27b5789d29942e1059a6fa0b8c1
anshul7z <anshul777.s@gmail.com>
2024-12-20 23:10:11 +0530

    created template for kafka producer and consumer.

---
+++ KafkaProducer/src/test/java/com/kafkaapp/KafkaProducer/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaapp.KafkaProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
